37d337dc54b8a8b986a429ef772da59c93bbf19f
Ivan Nesterov <85916405+gogoshbelichi@users.noreply.github.com>
2025-09-01 12:54:17 +0300

    python client (without virtual environment - did as in the guide)

---
+++ src/MeterReaderPython/MeterReaderPython.py
@@ -0,0 +1,27 @@
+from google.protobuf.timestamp_pb2 import Timestamp
+import grpc
+
+from meterservice_pb2 import ReadingMessage, ReadingPacket, ReadingStatus
+from meterservice_pb2_grpc import MeterReadingServiceStub
+
+def main():
+    print("Starting Client Call")
+
+    packet = ReadingPacket(Successful = ReadingStatus.Success)
+
+    now = Timestamp()
+    now.GetCurrentTime()
+
+    reading = ReadingMessage(CustomerId = 1, ReadingTime = now, ReadingValue = 10000)
+    packet.Readings.append(reading)
+
+    channel = grpc.insecure_channel("localhost:8888")
+    stub = MeterReadingServiceStub(channel)
+
+    response = stub.AddReading(packet)
+    if (response.Status == ReadingStatus.Success):
+        print("Succeeded")
+    else:
+        print("Failed")
+
+main()
